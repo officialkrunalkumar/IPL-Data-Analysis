3f1910f88f7181fcf3e7e9a4ea05eb85da8b87ed
Krunalkumar <krunalkumar@mountblue.io>
2021-06-01 11:49:11 +0530

    Menu Based Interface Added

---
+++ startup_script.py
@@ -0,0 +1,31 @@
+import os
+
+
+def menu():
+    print("Welcome to the IPL Analysis and Story Telling Project")
+    print("Please choose any one option at a time...")
+    print("1. Total Runs scored by team")
+    print("2. Top Batsman for Royal Challengers Bangalore")
+    print("3. Foreign Umpire Analysis")
+    print("4. Number of Games Played per Year")
+    choice = int(input("Enter your choice (1-4) :"))
+    if choice == 1:
+        os.system('python3 total_runs_scored_by_teams.py')
+    elif choice == 2:
+        os.system('python3 top_batsman_bangalore.py')
+    elif choice == 3:
+        os.system('python3 foreign_umpire_analysis.py')
+    elif choice == 4:
+        os.system('python3 matches_per_year.py')
+    else:
+        print("")
+        rechoice = ord(input("Wrong Choice! do you want to try again? (y/n)"))
+        if rechoice == 89 or rechoice == 121:
+            os.system('clear')
+            menu()
+        else:
+            print("Thanks for Using IPL Analysis and Story Telling Project")
+
+
+if __name__ == '__main__':
+    menu()
